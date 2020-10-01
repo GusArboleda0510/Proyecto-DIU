44f19553b077508843de834060c6fc4459ad8df0
AlejaBecerraB <67833288+AlejaBecerraB@users.noreply.github.com>
2020-10-01 01:30:41 -0500

    Commit Alejandra
    Ventana de Puntaje funcional e implementacion de XML

--- ProyectoDIU/src/Vista/Ganadores.java
+++ ProyectoDIU/src/Vista/Ganadores.java
@@ -9,12 +9,11 @@ import Control.ControlTXT;
 import Modelo.CreadordeDocs;
 import Modelo.LectordeDocs;
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -29,7 +28,7 @@ public class Ganadores extends javax.swing.JDialog {
     LectordeDocs ld;
     String tiempo, puntaje,nickName="ALEJANDRA";
     File ganadores = new File("persistencia/Ganadores.xml");
-    
+    NodeList lista;
     
     public Ganadores(java.awt.Frame parent, boolean modal) throws Exception {
         super(parent, modal);
@@ -46,9 +45,9 @@ public class Ganadores extends javax.swing.JDialog {
         tiempo=dato[0];
         puntaje=dato[2];
 //        nickName=dato[3];
-        generarContenido(doc.getDocumento(),doc.getElementoRaiz());
+//        generarContenido(doc.getDocumento(),doc.getElementoRaiz());
         obtenerContenido();//Falta obtener contenido para mostrarlo por la interfaz
-        System.out.println("FINAL");
+        
     }
     
     private void generarContenido(Document documento, Element raiz) throws Exception{
@@ -69,24 +68,96 @@ public class Ganadores extends javax.swing.JDialog {
         puestos.appendChild(timepo);
         doc.generarXML(ganadores, documento);
     }
-    public String[] obtenerContenido() throws Exception {
-        String[] nombres;
+    public void obtenerContenido() throws Exception {
+        
         ld = new LectordeDocs(ganadores);
         Document documento = ld.getDocumento();
-        NodeList lista = documento.getElementsByTagName("puestos");
-        nombres = new String[lista.getLength()];
-        
+        lista = documento.getElementsByTagName("puestos");
+        System.out.println(lista.getLength()+"....");
+        String[] datos= new String[lista.getLength()];
+//        datos = new String[lista.getLength()];
         System.out.println("Ganadanores");
         for (int n = 0; n < lista.getLength(); n++) {
             Node nodo = lista.item(n);
             if (nodo.getNodeType() == Node.ELEMENT_NODE) {
-                System.out.println(nodo.getNodeName());
-                Element ganadores = (Element) nodo;
-//                System.out.println("ganadores "+ ganadores.getElementsByTagName("puntaje").item(0).getTextContent());
+                Element element = (Element) nodo;
+//                System.out.println(element.getElementsByTagName("jugadores").item(0).getTextContent());
+//                System.out.println(element.getElementsByTagName("puntaje").item(0).getTextContent());
+//                System.out.println(element.getElementsByTagName("timepo").item(0).getTextContent());
+                datos[n]=element.getElementsByTagName("jugadores").item(0).getTextContent()
+                        +"-"+element.getElementsByTagName("puntaje").item(0).getTextContent()
+                        +"-"+element.getElementsByTagName("timepo").item(0).getTextContent();
+//                
+            }
+            
+        }
+        for (int i = 0; i < datos.length; i++) {
+//                System.out.println(datos[i]);
+                
+            }
+        ordenarXML(datos);
+    }
+    public void ordenarXML(String[] datos){
+        String[] aux=null;
+        int[] puntaje=new int[datos.length];
+        int[] temporal=new int[datos.length];
+        
+        for (int i = 0; i < datos.length; i++) {
+            System.out.println(datos[i]);
+            aux=datos[i].split("-");
+            puntaje[i]=Integer.parseInt(aux[1]);
+            temporal[i]=Integer.parseInt(aux[1]);
+        }
+        int[] posiciones=ordenamiento(puntaje, mayorMenos(temporal));
+        
+        for (int i = 0; i < datos.length; i++) {
+            jTextArea1.append(""+(i+1)+"   ");
+            String[] aux01=datos[posiciones[i]].split("-");
+            for (int j = 0; j < aux01.length; j++) {
+            
+            jTextArea1.append(" "+aux01[j] +"   ");
+                
+            }
+            jTextArea1.append("\n");
+        }
+    }
+    public int[] ordenamiento (int[] p , int[]ordendado){
+        System.out.println(" ");
+        int[]posiciones = new int[p.length];
+                for (int i = 0; i < p.length; i++) {
+//            System.out.print(p[i]+" -- ");
+
+            for (int j = 0; j < p.length; j++) {
+                    System.out.println("preuba  "+ ordendado[i] + "== " +"p " + p[j]);
+                if(ordendado[i] == p[j]){
+//                    System.out.println("-- " + datos[i]+"--");
+                    System.out.println(j);
+                    posiciones[i]=j;
+                    break;
+                }
             }
         }
-        return nombres;
+                return posiciones;
     }
+    public int[] mayorMenos(int[] a) {  
+    int[] aux=null;
+    int menor;
+    for (int x = 0; x < a.length; x++) {
+        for (int i = 0; i < a.length-x-1; i++) {
+            if(a[i] < a[i+1]){
+                int tmp = a[i+1];
+                a[i+1] = a[i];
+                a[i] = tmp;
+            }
+        }
+    }
+    System.out.println("ORDENADO");
+    for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+"-");
+//            
+        }
+    return aux=a;
+}
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -105,12 +176,17 @@ public class Ganadores extends javax.swing.JDialog {
         jLabel1.setText("GANADORES");
 
         jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Tempus Sans ITC", 3, 24)); // NOI18N
+        jTextArea1.setFont(new java.awt.Font("Tempus Sans ITC", 3, 14)); // NOI18N
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
         jButton1.setFont(new java.awt.Font("Tempus Sans ITC", 1, 24)); // NOI18N
         jButton1.setText("SALIR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -155,6 +231,10 @@ public class Ganadores extends javax.swing.JDialog {
         setSize(new java.awt.Dimension(1014, 690));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     public static void main(String[] args) throws Exception {
         new Ganadores(null, true);
